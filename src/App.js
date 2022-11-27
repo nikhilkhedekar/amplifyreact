9d045f73ff6758846468459bbf07d316158e7a04
nikhilkhedekar <nikhilkhedekar2012@gmail.com>
2022-11-27 22:24:09 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import DeleteUserData from './deleteUserData';
+import GetUserData from './getUserData';
+import PatchUserData from './patchUserData';
+import PostUserData from './postUserData';
+import PutUserData from './putUserData';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostUserData />
+      <PutUserData />
+      <PatchUserData />
+      <GetUserData />
+      <DeleteUserData />
     </div>
   );
 }
---
+++ src/getUserData.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import axios from "axios";
+
+const GetUserData = () => {
+    const [id, setId] = useState('') 
+    const onSubmitHandler = async () => {
+        const getUserData = await axios.get("https://2ir65j8wv8.execute-api.ap-south-1.amazonaws.com/api/v1/user/userData", { _id: id });
+        console.log("getUserData", getUserData);
+    }
+    return (
+        <>
+            <input value={id} onChange={(e) => setId(e.target.value)} type="text" />
+            <button onClick={onSubmitHandler} > Get User </button>
+        </>
+    )
+}
+
+export default GetUserData
